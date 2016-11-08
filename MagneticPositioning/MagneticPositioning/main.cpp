39da262593800b1474ff0370c1595b265fb43983
RogerTan <tankaitian@gmail.com>
2016-11-08 15:46:22 +0800

    add new file

---
+++ MagneticPositioning/MagneticPositioning/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MagneticPositioning
+//
+//  Created by Kaitian Tan on 11/8/16.
+//  Copyright © 2016 Mecon Tan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
